3422cb54ed1151ca6fd0f53787a86a8d5b4f825e
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2024-12-24 14:21:32 +0530

    initial commit

---
+++ arraylist/src/com/vaishnavi/cab/booking/model/Users.java
@@ -0,0 +1,57 @@
+package com.vaishnavi.cab.booking.model;
+
+
+import java.util.Objects;
+
+public class Users {
+    private int userid;
+    private String name;
+    private String email;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userid +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+
+    }
+}
+
+
